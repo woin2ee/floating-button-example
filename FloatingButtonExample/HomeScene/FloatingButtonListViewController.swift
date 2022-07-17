4c9472acb70aa1e03bdd2e44a70acf67ba01407e
Jaewon <woin2ee@gmail.com>
2022-07-17 17:32:20 +0900

    feat : Add FloatingButtonListViewController

---
+++ FloatingButtonExample/HomeScene/FloatingButtonListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FloatingButtonListViewController.swift
+//  FloatingButtonExample
+//
+//  Created by Jaewon on 2022/07/17.
+//
+
+import UIKit
+
+final class FloatingButtonListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func didTapAddButton(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+}
--- FloatingButtonExample/HomeScene/HomeViewController.swift
+++ FloatingButtonExample/HomeScene/HomeViewController.swift
@@ -12,4 +12,14 @@ final class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func didTapAddButton(_ sender: UIButton) {
+        let storyboardID = String(describing: FloatingButtonListViewController.self)
+        guard let floatingButtonListVC = storyboard?.instantiateViewController(withIdentifier: storyboardID) as? FloatingButtonListViewController else { return }
+        
+        floatingButtonListVC.modalTransitionStyle = .crossDissolve
+        floatingButtonListVC.modalPresentationStyle = .overCurrentContext
+        
+        present(floatingButtonListVC, animated: true)
+    }
 }
